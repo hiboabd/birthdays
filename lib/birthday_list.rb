9635c293e53533f85448859ce911d1c37f5346ef
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-04-18 09:09:59 +0100

    Add store_birthday method

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,9 @@
+class BirthdayDatabase
+  def store_birthday(friend, day, month, year)
+    "Stored #{friend}: #{day} #{month} #{year}"
+  end
 
+  def print_birthday(database)
+
+  end
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,24 @@
+require 'birthday_list'
 
+describe BirthdayDatabase do
+  it 'check for store_birthday method' do
+    expect(subject).to respond_to(:store_birthday)
+  end
+
+  it 'store_birthday takes three arguments' do
+    expect(subject).to respond_to(:store_birthday).with(4).arguments
+  end
+
+  it 'store_birthday method returns store message' do
+    expect(subject.store_birthday("James", "23", "March", "1990")).to eq "Stored James: 23 March 1990"
+  end
+
+  it 'print_birthday method takes one argument' do
+    expect(subject).to respond_to(:print_birthday).with(1).argument
+  end
+
+#  it 'print_birthday method returns friend' do
+#    subject.store_birthday("James", "23", "March", "1990")
+#    expect(subject.print_birthday(friend)).to eq "James"
+#  end
+end
